327163f795e53ea0b75cdb1089d75f5cfca6cf4d
Aloys <aloys.portafaix@gmail.com>
2018-10-11 14:59:32 -0400

    Dice
    Enables user to pick how many times they wish to roll (between 1-3). Rolls dice and displays percentage of how many time each side comes up. Main file makes a dozen calls to pick how many time to roll and rolls the dice and then displays these percentages.

---
+++ Dice/Dice.h
@@ -0,0 +1,18 @@
+//
+// Created by Aloys on 18-10-11.
+//
+
+#ifndef UNTITLED_DICE_H
+#define UNTITLED_DICE_H
+
+class Dice {
+
+public:
+    int OneRoll();
+    int NumberOfDice();
+    int *Rolls();
+    void RollPercentage(int x);
+    void DisplayPercentage();
+};
+
+#endif //UNTITLED_DICE_H
---
+++ Dice/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Dice.h"
+
+int main() {
+
+    Dice dice;
+
+    dice.Rolls();
+    dice.Rolls();
+    dice.Rolls();
+    dice.Rolls();
+    dice.Rolls();
+    dice.Rolls();
+    dice.Rolls();
+    dice.Rolls();
+    dice.DisplayPercentage();
+
+    return 0;
+}
