7fdbc52097eafc481368227319c6119d61290f56
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2018-10-14 23:37:06 -0400

    numOfCards now equals the number of countries in the map

--- src/Main.cpp
+++ src/Main.cpp
@@ -31,7 +31,7 @@ int main() {
 
     cout << "************************* \n CARDS, DECK, HAND DEMO \n*************************\n";
 
-	numOfCards = 50; // to be replaced with number of continents
+	numOfCards = loader.getMap().getTerritory().size(); //the number of countries in the map
 
 	Deck deck(numOfCards);
 
