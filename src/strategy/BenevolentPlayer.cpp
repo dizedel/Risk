bd14110c5f71ce2dbd01a3afa26082b318d567bd
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2018-11-18 21:18:17 -0500

    Added BenevolentPlayer's Attack

--- src/strategy/BenevolentPlayer.cpp
+++ src/strategy/BenevolentPlayer.cpp
@@ -1,13 +1,16 @@
 //
 // Created by pamel on 2018-11-18.
 //
-
+#include <iostream>
 #include "strategy/BenevolentPlayer.h"
+
+using namespace std;
+
 void BenevolentPlayer::reinforce(Player* p) {
 
 }
 void BenevolentPlayer::attack(Player* p) {
-
+    cout << "The player does not wish to attack." << endl;
 }
 void BenevolentPlayer::fortify(Player* p) {
 
